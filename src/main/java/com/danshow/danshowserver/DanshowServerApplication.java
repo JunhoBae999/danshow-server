5d4c4ba6346f9656ab491deaccbf17ea537fe540
Junho Bae <junho918@naver.com>
2021-03-30 22:49:56 +0900

    initial commit

---
+++ src/main/java/com/danshow/danshowserver/DanshowServerApplication.java
@@ -0,0 +1,13 @@
+package com.danshow.danshowserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanshowServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanshowServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danshow/danshowserver/DanshowServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danshow.danshowserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanshowServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
