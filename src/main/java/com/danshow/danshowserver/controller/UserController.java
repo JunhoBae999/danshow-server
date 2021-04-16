73fa784f6f3d0de0c533ca9e5b765fa45b26d798
Junho Bae <junho918@naver.com>
2021-04-17 00:25:51 +0900

    [ADD] Swagger Configuration 작성, 의존성 추가, UserController api 문서화 적용

--- src/main/java/com/danshow/danshowserver/controller/UserController.java
+++ src/main/java/com/danshow/danshowserver/controller/UserController.java
@@ -4,6 +4,8 @@ import com.danshow.danshowserver.service.DancerService;
 import com.danshow.danshowserver.service.MemberService;
 import com.danshow.danshowserver.web.dto.DancerUpdateRequestDto;
 import com.danshow.danshowserver.web.dto.MemberUpdateRequestDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = {"1.User"})
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -20,12 +23,14 @@ public class UserController {
     private final MemberService memberService;
     private final DancerService dancerService;
 
+    @ApiOperation(value = "멤버 업데이트" , notes = "멤버의 정보를 업데이트 합니다.")
     @PostMapping("user/member-update")
     public ResponseEntity memberUpdate(@RequestBody MemberUpdateRequestDto updateRequestDto) {
         memberService.update(updateRequestDto);
         return new ResponseEntity<>("success", HttpStatus.OK);
     }
 
+    @ApiOperation(value = "멤버 전환",notes = "멤버를 안무가로 전환합니다.")
     @PostMapping("user/member-to-dancer")
     public ResponseEntity memberToDancer(@RequestParam String email) {
         Long result = memberService.toDancer(email);
@@ -34,6 +39,7 @@ public class UserController {
         return new ResponseEntity<>("already dancer", HttpStatus.ACCEPTED);
     }
 
+    @ApiOperation(value = "안무가 업데이트",notes = "안무가의 정보를 업데이트합니다.")
     @PostMapping("user/dancer-update")
     public ResponseEntity dancerUpdate(@RequestBody DancerUpdateRequestDto updateRequestDto) {
         dancerService.update(updateRequestDto);
