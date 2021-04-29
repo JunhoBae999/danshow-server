f1217c858de487e6cf029b5f845c96d8faec60c7
plan222 <52143360+plan222@users.noreply.github.com>
2021-04-29 17:40:41 +0900

    [REFACTORING] 복합키 equals, hashCode 오버라이드
    MemberCrewId와 MemberVideoId의 equals와 hashCode를 오버라이드 하였습니다.
    +크루 빠진 파일

--- src/main/java/com/danshow/danshowserver/domain/composite/MemberCrewId.java
+++ src/main/java/com/danshow/danshowserver/domain/composite/MemberCrewId.java
@@ -1,8 +1,38 @@
 package com.danshow.danshowserver.domain.composite;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@Getter
+@NoArgsConstructor
 public class MemberCrewId implements Serializable {
     private String member;
     private String crew;
+
+    public MemberCrewId(String member, String crew) {
+        this.member = member;
+        this.crew = crew;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj)
+            return true;
+        MemberCrewId memberCrewId = (MemberCrewId) obj;
+        return (this.getMember().equals(memberCrewId.getMember()) && this.getCrew().equals(memberCrewId.getCrew()));
+    }
+
+    @Override
+    public int hashCode() {
+        int h = 0;
+        String concat = member.concat(getCrew());
+        if(concat.length() > 0) {
+            for(char ch : concat.toCharArray()) {
+                h = 31 * h + ch;
+            }
+        }
+        return h;
+    }
 }
