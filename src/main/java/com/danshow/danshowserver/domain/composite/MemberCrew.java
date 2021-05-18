d3226f784b10d4bf4e348a4884ce93dba6b849c8
plan222 <52143360+plan222@users.noreply.github.com>
2021-05-18 12:30:24 +0900

    [DELETE] MemberToVideoPost
    준호님이 비디오 관련 클래스 만들어서 필요 없어져서 삭제합니다.
    MemberCrew는 id 를 복합키 방식에서 Id 하나인거로 수정했습니다.

--- src/main/java/com/danshow/danshowserver/domain/composite/MemberCrew.java
+++ src/main/java/com/danshow/danshowserver/domain/composite/MemberCrew.java
@@ -10,17 +10,34 @@ import javax.persistence.*;
 
 @Getter
 @Table(name = "Member_crew")
-@IdClass(MemberCrewId.class)
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberCrew {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_crew_id")
+    private Long id;
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private Member member;
 
-    @Id
     @ManyToOne
     @JoinColumn(name = "crew_id")
     private Crew crew;
+
+    public void setMember(Member member) {
+        this.member = member;
+        if(!member.getCrewList().contains(this)) {
+            member.getCrewList().add(this);
+        }
+    }
+
+    public void setCrew(Crew crew) {
+        this.crew = crew;
+        if(!crew.getMemberList().contains(this)) {
+            crew.getMemberList().add(this);
+        }
+    }
 }
